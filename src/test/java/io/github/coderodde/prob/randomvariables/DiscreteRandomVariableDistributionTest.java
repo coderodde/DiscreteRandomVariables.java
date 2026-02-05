41e1e9806cb31eddc3559f7ebb2b8beaf4094ad6
Rodion Efremov <coderodd3@yandex.com>
2026-02-05 11:32:31 +0200

    Better test coverage for binomial(...).

--- src/test/java/io/github/coderodde/prob/randomvariables/DiscreteRandomVariableDistributionTest.java
+++ src/test/java/io/github/coderodde/prob/randomvariables/DiscreteRandomVariableDistributionTest.java
@@ -40,13 +40,31 @@ public class DiscreteRandomVariableDistributionTest {
                 .checkSuccessProbability(BigDecimal.ONE);
     }
     
-    @Test
-    public void binomial_1_on_n_0() {
-        assertEquals(BigInteger.ONE, binomial(BigInteger.ZERO, BigInteger.ONE));
+    @Test(expected = IllegalArgumentException.class)
+    public void binomialThrowsOnNegativeN() {
+        binomial(BigInteger.valueOf(-1L), BigInteger.ONE);
     }
     
     @Test
-    public void binomial_1_on_k_0() {
+    public void binomial_0_1() {
+        assertEquals(BigInteger.ZERO, 
+                     binomial(BigInteger.ZERO, BigInteger.ONE));
+    }
+    
+    @Test
+    public void binomial_1_minus_1() {
+        assertEquals(BigInteger.ZERO,
+                     binomial(BigInteger.ONE, BigInteger.ONE.negate()));
+    }
+    
+    @Test
+    public void binomialZeroK() {
         assertEquals(BigInteger.ONE, binomial(BigInteger.ONE, BigInteger.ZERO));
+    }
+    
+    @Test
+    public void binomialNequalsK() {
+        BigInteger a = BigInteger.ZERO;
+        assertEquals(BigInteger.ONE, binomial(a, a));
     }
 }
