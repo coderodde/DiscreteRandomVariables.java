5f728bf6c16d82033c6786bf322215cfa3481138
Rodion Efremov <coderodd3@yandex.com>
2026-02-05 11:52:52 +0200

    All distributions are fixed and 100% test covered.

--- src/test/java/io/github/coderodde/prob/randomvariables/support/HypergeometricDistributionTest.java
+++ src/test/java/io/github/coderodde/prob/randomvariables/support/HypergeometricDistributionTest.java
@@ -20,4 +20,44 @@ public class HypergeometricDistributionTest {
                 dist.getProbabilityMass(BigInteger.valueOf(2)).doubleValue(), 
                 0.01);
     }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void nLessThanZero() {
+        new HypergeometricDistribution(
+                BigInteger.valueOf(-1), 
+                BigInteger.ONE, 
+                BigInteger.ONE);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void kLessThanZero() {
+        new HypergeometricDistribution(
+                BigInteger.valueOf(2), 
+                BigInteger.valueOf(-1), 
+                BigInteger.ONE);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void smallNLessThanZero() {
+        new HypergeometricDistribution(
+                BigInteger.valueOf(2), 
+                BigInteger.ONE, 
+                BigInteger.valueOf(-1));
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void kLargerThanN() {
+        new HypergeometricDistribution(
+                BigInteger.valueOf(2), 
+                BigInteger.valueOf(3),
+                BigInteger.ONE);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void smallNLargerThanN() {
+        new HypergeometricDistribution(
+                BigInteger.valueOf(3), 
+                BigInteger.ONE, 
+                BigInteger.valueOf(4));
+    }
 }
